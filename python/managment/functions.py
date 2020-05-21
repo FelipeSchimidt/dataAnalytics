e439e3d119a022ef572e001968a19fbf4091a3b1
FelipeSchimidt <felipeschimidt85@gmail.com>
2020-05-21 17:27:13 -0300

    tentativa 2 para desenvolvimento de base de codificação com TDD

--- python/managment/functions.py
+++ python/managment/functions.py
@@ -1,9 +1,15 @@
+import os
 
-class Managment_functions:
 
-    def read_files(self, paths=''):
-        # print(type(paths))
-        with open(paths, 'r') as path_files:
-            return type(path_files)
-            """ for files in path_files:
-                print(files) """
+def read_files(self, paths):
+    if paths != 'str':
+        return 'Arquivo não encontrado'
+    with open(paths) as paths_files:
+        files = os.read(paths_files)
+        print(type(files))
+        # for files in paths_files:
+        #    print(type(files))
+        #    return files
+
+
+read_files('C:\Developer\dataAnalytics\dados\arquivo_teste_02.wnd')
--- python/managment/test_functions.py
+++ python/managment/test_functions.py
@@ -1,11 +1,16 @@
-from functions import Managment_functions
 import unittest
+import functions
 
-file_test = '"TOA5","CHUI 1","CR1000","54696","CR1000.Std.27.04","CPU:Estacv03_CHUI 1.CR1","35897","Table1"'
+file_test = ["TOA5", "CHUI 1", "CR1000", "54696",
+             "CR1000.Std.27.04", "CPU:Estacv03_CHUI 1.CR1", "35897", "Table1"]
 
 
 class TestFunctions(unittest.TestCase):
 
-    def test_type_files(self):
-        self.assertTrue(Managment_functions.read_files(
-            self, 'C://Developer//ataAnalytics//dados//arquivo_teste_02.wnd') == 'str')
+    def test_type_not_files(self):
+        self.assertFalse(
+            expr=(file_test == functions.read_files(self, 'C:\Developer\dataAnalytics\dados')))
+
+    def test_out_read_files(self):
+        self.assertIs(type(functions.read_files(
+            self, 'C:\Developer\dataAnalytics\dados')), type(file_test))

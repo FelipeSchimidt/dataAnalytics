f6980695f1ee6f59d5e38a8fd0c893666a048d0f
FelipeSchimidt <felipeschimidt85@gmail.com>
2020-05-28 16:50:20 -0300

    teste de flask como rest api ok, inicio do desenvolvimento api da aplicação

--- python/api/server.py
+++ python/api/server.py
@@ -1,26 +1,33 @@
-from flask import Flask, jsonify, request, response
+from flask import Flask, jsonify, request
+
+from controllers.Stations import StationController
 
 app = Flask('__name__')
 
-dados = [
+pessoal = [
     {
+        "id": 1,
         "first_name": "Felipe",
         "last_name": "Santos",
-        "function": "Analista de dados"
+        "function": "Analista de pessoal"
     },
     {
+        "id": 2,
         "first_name": "Lizyane",
         "last_name": "Santos",
         "function": "professora"
     }, {
+        "id": 3,
         "first_name": "Joene",
         "last_name": "Santos",
         "function": "psicologa"
     }, {
+        "id": 4,
         "first_name": "Valquiria",
         "last_name": "Santos",
         "function": "Contas a pagar"
     }, {
+        "id": 5,
         "first_name": "Alfredo",
         "last_name": "Santos",
         "function": "Dieretor Administrativo"
@@ -30,13 +37,55 @@ dados = [
 
 @app.route('/', methods=['GET'])
 def index():
-    datas = [data for data in response.get_json()]
-    return 'micro servidor ativo'
+    # datas = [data for data in pessoal]
+    return jsonify(pessoal), 200
 
 
-@app.route('/new', methods=['POST'])
-def new():
-    return jsonify(data), 201
+@app.route('/person/functions/<string:function>', methods=['GET'])
+def person_per_function(function):
+    functions = [func for func in pessoal if func['function'] == function]
+    return jsonify(functions), 200
+
+
+@app.route('/person/id/<int:id>', methods=['GET'])
+def person_per_id(id):
+    for pessoas in pessoal:
+        if pessoas['id'] == id:
+            return jsonify(pessoas)
+
+
+@app.route('/person/id/<int:id>', methods=['PUT'])
+def update_function(id):
+    for pessoas in pessoal:
+        if pessoas['id'] == id:
+            pessoas['function'] = request.get_json().get('function')
+
+            return jsonify(pessoas), 200
+
+    return jsonify({'error': 'registro não encontrado'}), 404
+
+
+@app.route('/person/new', methods=['POST'])
+def new_person():
+    novo = request.get_json()
+    pessoal.append(novo)
+
+    return jsonify(novo), 201
+
+
+@app.route('/person/id/<int:id>', methods=['DELETE'])
+def delete_person(id):
+    for pessoas in pessoal:
+        if pessoas['id'] == id:
+            pessoal.pop(id-1)
+            return jsonify({'message': 'Pessoa deletada'}), 200
+
+    return jsonify({'error': 'Pessoa não encontrada'}), 404
+
+
+@app.route('/stations', methods=['GET'])
+def stations():
+    return StationController.index(self)
 
 
 if __name__ == '__main__':
---
+++ python/controllers/Stations.py
@@ -0,0 +1,47 @@
+import pyrebase
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+from flask import jsonify
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+config = {
+    "apiKey": os.getenv("ENV_API_KEY"),
+    "authDomain": os.getenv("ENV_AUTH_DOMAIN"),
+    "databaseURL": os.getenv("ENV_DATABASE_URL"),
+    "storageBucket": os.getenv("ENV_STORAGE_BUCKET")
+}
+
+""" firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+# data = {"station_name": "chui 4", "station_id": 266}
+
+db.child("stations").push(data)
+ """
+
+
+class StationController:
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+
+    def __init__(self, db):
+        db = self.db
+
+    def index(self):
+        stations = self.db.child('stations').get()
+        print(stations.val())
+        # return jsonify(stations.val())
+
+    def show(self):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass

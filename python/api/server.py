16b257e642e65c391851d6eb9cfeea82c23c32cb
Felipe Santos <felipeschimidt85@gmail.com>
2020-05-28 09:44:28 -0300

    criação do método post e index da api

--- python/api/server.py
+++ python/api/server.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify, request, response
 
 app = Flask('__name__')
 
+dados = [
+    {
+        "first_name": "Felipe",
+        "last_name": "Santos",
+        "function": "Analista de dados"
+    },
+    {
+        "first_name": "Lizyane",
+        "last_name": "Santos",
+        "function": "professora"
+    }, {
+        "first_name": "Joene",
+        "last_name": "Santos",
+        "function": "psicologa"
+    }, {
+        "first_name": "Valquiria",
+        "last_name": "Santos",
+        "function": "Contas a pagar"
+    }, {
+        "first_name": "Alfredo",
+        "last_name": "Santos",
+        "function": "Dieretor Administrativo"
+    }
+]
 
-@app.route('/')
-def home():
+
+@app.route('/', methods=['GET'])
+def index():
+    datas = [data for data in response.get_json()]
     return 'micro servidor ativo'
+
+
+@app.route('/new', methods=['POST'])
+def new():
+    return jsonify(data), 201
 
 
 if __name__ == '__main__':
